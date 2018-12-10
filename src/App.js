691f969f956ddf979e1ab25901cf09daf897950c
Josh Hall <joshhall90@yahoo.com>
2018-12-10 13:06:48 -0500

    Made a book array with propertties, called them with JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let books = [
+  {
+    'title': 'The Grinch',
+    'id': 101,
+    'author': 'Dr. Seuss',
+    'url': 'https://www.imdb.com/title/tt0170016/'
+  },
+  {
+    'title': 'Red Fish Blue fish',
+    'id': 102,
+    'author': 'Dr. Seuss',
+    'url': 'https://www.amazon.com/Fish-Blue-Myself-Beginner-Books/dp/0756921333'
+  },
+  {
+    'title': 'Green Eggs and Ham',
+    'id': 103,
+    'author': 'Dr. Seuss',
+    'url': 'https://www.amazon.com/Green-Eggs-Ham-Dr-Seuss/dp/0394800168'
+  }
+];
+
 class App extends Component {
   render() {
+    // this is javascript
+    // const s = 'Hello, World!';
+
+    // below this line,within the RETURN is JSX
     return (
       <div className="App">
-        
+        <h2>{this.props.name}</h2>
+        {/* Ay yo bish, this is a JSX Comment */}
+        {books.map(book =>
+            <div key={book.id}>
+              <span>
+                <a href={book.url} target= "_blank">{book.title}</a> -
+              </span>
+              <span>{book.author}</span>
+            </div>
+          )}
       </div>
-    );
+    ); // this is javascript again
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App />, document.getElementById('root'));
+  <App name={'Dr. Seuss'} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+
+// stops page from refreshing, however it still re-renders components
+if (module.hot) {
+  module.hot.accept();
+}
